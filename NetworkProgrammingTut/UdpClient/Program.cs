7ac40b6bef9a91c5184b93c7d059a1b5900ff029
Pitsanu-T <pitsanu.t@carpass.co.th>
2014-08-04 17:35:16 +0700

    UDP project
    Signed-off-by: Pitsanu-T <pitsanu.t@carpass.co.th>

---
+++ NetworkProgrammingTut/UdpClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UdpClient
+{
+    class Program
+    {
+        private const int listenPort = 11000;
+
+        static void StartClient()
+        {
+            AddressFamily addressFamily = AddressFamily.InterNetwork;
+            System.Net.Sockets.UdpClient client = new System.Net.Sockets.UdpClient();        
+            IPAddress ipAddress = Dns.GetHostAddresses(Dns.GetHostName()).Where(ip => ip.AddressFamily == addressFamily).FirstOrDefault();
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, listenPort);
+
+            client.Connect(localEndPoint);
+            while (true)
+            {
+                Console.Write("Input: ");
+
+                string str = Console.ReadLine();
+                byte[] data = Encoding.ASCII.GetBytes(str);
+
+                client.Send(data, data.Length);
+                
+            }
+        }
+
+        static int Main(string[] args)
+        {
+            StartClient();
+
+            return 0;
+        }
+    }
+}
---
+++ NetworkProgrammingTut/UdpServer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace UdpServer
+{
+    class Program
+    {
+        private const int listenPort = 11000;
+
+        static void StartListener()
+        {
+            AddressFamily addressFamily = AddressFamily.InterNetwork;
+            bool done = false;
+
+            UdpClient listener = new UdpClient();
+            IPAddress ipAddress = Dns.GetHostAddresses(Dns.GetHostName()).Where(ip => ip.AddressFamily == addressFamily).FirstOrDefault();
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, listenPort);
+
+     
+            try
+            {
+                while (!done)
+                {
+                    byte[] data = listener.Receive(ref localEndPoint);
+                    
+                    string str = Encoding.ASCII.GetString(data);
+
+                    Console.WriteLine(str);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                listener.Close();
+            }
+            
+        }
+
+        static int Main(string[] args)
+        {
+            StartListener();
+
+            return 0;
+        }
+    }
+}
